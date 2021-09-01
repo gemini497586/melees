d943eefc8e2dfa0e8b8ddbaa374f8a29b36fbef7
ryanj8512 <ryanjan8512@gmail.com>
2021-09-01 22:48:03 +0800

    起手式不重要

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import './App.css'
+import './style/global.css'
 import Header from './component/Header.js'
 import MinorBar from './component/MinorBar.js'
+import Footer from './component/Footer.js'
+import FeatureIndex from './pages/feature/FeatureIndex'
+import FeatureContentImg from './pages/feature/component/FeatureContentImg'
+import FeatureContentIntro from './pages/feature/component/FeatureContentIntro'
+import FeatureWeek from './pages/feature/component/FeatureWeek'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MinorBar />
+      <FeatureIndex />
+      <FeatureContentImg />
+      <FeatureContentIntro />
+      <FeatureWeek />
+      <Footer />
     </div>
   )
 }
