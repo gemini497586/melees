3a1ad0c5079c5cdeef67af622849a0b50c650a4a
ryanj8512 <ryanjan8512@gmail.com>
2021-09-02 16:55:55 +0800

    建立按讚數和瀏覽數.js

---
+++ frontend/src/component/HeartViewNum.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../style/heartViewNum.css'
+import '../style/featureComponent.css'
+
+function HeartViewNum() {
+  return (
+    <>
+      <div className="hvn-size fcolor-grey-800">
+        <div className="hvn-flex">
+          <div className=" d-flex align-items-center">
+            <span>
+              <i className="fas fa-heart fa-fw ficon-size me-2"></i>
+            </span>
+            <span className="font-400S">30322</span>
+          </div>
+          <div className="fline-g500"></div>
+          <div className="d-flex align-items-center">
+            <span>
+              <i className="fas fa-eye fa-fw ficon-size me-2"></i>
+            </span>
+            <span className="font-400S">30322</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default HeartViewNum
