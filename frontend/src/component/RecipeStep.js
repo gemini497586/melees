2ec00f6be1c115c06b8185481da70eceac779751
meleelin <sdy2615380450@gmail.com>
2021-09-02 14:19:47 +0800

    新增recipeStep

---
+++ frontend/src/component/RecipeStep.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import '../style/recipeStep.css'
+
+function RecipeStep() {
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-10">
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">1.</span>
+              <span className="font-400L recipeStep-content">
+                三層肉，用高粱或米酒，豬肉用酒洗乾淨，備料。
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">2.</span>
+              <span className="font-400L recipeStep-content">
+                鍋子開火，各一匙，胡椒鹽，黑胡椒，鹽，五香粉，蒜末，花椒，一起下鍋，煸出香味後
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">3.</span>
+              <span className="font-400L recipeStep-content">
+                綜合粉起鍋，三層肉再雙面沾滿醃製粉，包覆好，放置冰箱冷凍約1～2天。
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">4.</span>
+              <span className="font-400L recipeStep-content">
+                依照肉的厚度，大小，調整冷藏時間與醃製料多寡
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">5.</span>
+              <span className="font-400L recipeStep-content">
+                料理前，冷凍鹹豬肉退至常溫狀況，把沾附在表層的粉，稍微撥乾淨，
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">6.</span>
+              <span className="font-400L recipeStep-content">
+                放入鍋內，開中小火，蓋上鍋蓋悶煎，豬肉煎熟，表面呈現微焦狀，起鍋，備料。
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">7.</span>
+              <span className="font-400L recipeStep-content">
+                可以用筷子插肉測試，如果沒有血水溢出就是熟了。
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">8.</span>
+              <span className="font-400L recipeStep-content">
+                花椰菜，玉米筍，蔬菜稍微過熱水，花椰菜變深綠色後，起鍋瀝乾水份。
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">9.</span>
+              <span className="font-400L recipeStep-content">
+                鍋子清洗乾淨下一匙油，熱鍋後，紅椒，下鍋，稍微乾煎一下，
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">10.</span>
+              <span className="font-400L recipeStep-content">
+                加入花椰菜，玉米筍些許鹽巴，水，拌炒均勻，關火。
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+            <div className="d-flex recipeStep">
+              <span className="font-700M recipeStep-num">11.</span>
+              <span className="font-400L recipeStep-content">
+                鍋子開火，各一匙，胡椒鹽，黑胡椒，鹽，五香粉，蒜末，花椒，一起下鍋，煸出香味後
+              </span>
+            </div>
+            <hr className="recipeStep-divider w-100" />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default RecipeStep
