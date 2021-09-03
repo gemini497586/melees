b613141701b5d1afc462e8f1a0ac91101c39a3cd
hhhhungc <dial012@yahoo.com.tw>
2021-09-04 00:51:33 +0800

    做手機版footer

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import '../style/footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './FontawsomeIcons'
 
 function Footer() {
+  const content = ['關於我們', '加入MELEEs', '異業合作']
+  console.log(content)
   const bottom = [
     '隱私權政策',
     '使用者條款',
@@ -14,119 +18,127 @@ function Footer() {
       <footer className="footer">
         <div className="footer-mask">
           <div className="footer-top">
-            <div className="d-flex">
-              {/* Column1 */}
-              <div className="col">
-                <div className="font-700M footer-title">關於MELLEs</div>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="#/">關於我們</a>
-                  </li>
-                  <li>
-                    <a href="#/">加入MELEEs</a>
-                  </li>
-                  <li>
-                    <a href="#/">異業合作</a>
-                  </li>
-                </ul>
-              </div>
-              {/* Column2 */}
-              <div className="col">
-                <div className="font-700M footer-title">客製化便當</div>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="#/">代謝率計算</a>
-                  </li>
-                  <li>
-                    <a href="#/">熱量計算</a>
-                  </li>
-                </ul>
-              </div>
-              {/* Column3 */}
-              <div className="col">
-                <div className="font-700M footer-title">客戶服務</div>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="#/">進度查詢</a>
-                  </li>
-                  <li>
-                    <a href="#/">取消訂單</a>
-                  </li>
-                  <li>
-                    <a href="#/">追蹤清單</a>
-                  </li>
-                  <li>
-                    <a href="#/">常見問答</a>
-                  </li>
-                  <li>
-                    <a href="#/">聯絡我們</a>
-                  </li>
-                </ul>
-              </div>
-              {/* Column4 */}
-              <div className="col">
-                <div className="font-700M footer-title">購物商城</div>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="#/">生鮮食材</a>
-                  </li>
-                  <li>
-                    <a href="#/">調味料</a>
-                  </li>
-                  <li>
-                    <a href="#/">廚房器具</a>
-                  </li>
-                </ul>
-              </div>
-              {/* Column5 */}
-              <div className="col">
-                <div className="font-700M footer-title">精選食譜</div>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="#/">健康長肉肉</a>
-                  </li>
-                  <li>
-                    <a href="#/">健康不吃肉</a>
-                  </li>
-                  <li>
-                    <a href="#/">一周不煩惱</a>
-                  </li>
-                  <li>
-                    <a href="#/">家常好手藝</a>
-                  </li>
-                </ul>
-              </div>
-              {/* Column6 */}
-              <div className="col">
-                <div className="font-700M footer-title">私藏食譜</div>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="#/">熱門食譜</a>
-                  </li>
-                  <li>
-                    <a href="#/">上傳食譜</a>
-                  </li>
-                </ul>
-              </div>
+            {/* Column1 */}
+            <div className="col-12 col-md-2">
+              <dic className="footer-title">
+                <div className="font-700M">關於MELLEs</div>
+                <div className="">
+                  <FontAwesomeIcon icon="plus" className="plus font-400SS" />
+                </div>
+              </dic>
+              <ul className="list-unstyled">
+                {content.map((v, i) => {
+                  return (
+                    <li>
+                      <a href="#/">{v}</a>
+                    </li>
+                  )
+                })}
+                {/* <li>
+                  <a href="#/">關於我們</a>
+                </li>
+                <li>
+                  <a href="#/">加入MELEEs</a>
+                </li>
+                <li>
+                  <a href="#/">異業合作</a>
+                </li> */}
+              </ul>
+            </div>
+            {/* Column2 */}
+            <div className="col-12 col-md-2">
+              <div className="font-700M footer-title">客製化便當</div>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#/">代謝率計算</a>
+                </li>
+                <li>
+                  <a href="#/">熱量計算</a>
+                </li>
+              </ul>
+            </div>
+            {/* Column3 */}
+            <div className="col-12 col-md-2">
+              <div className="font-700M footer-title">客戶服務</div>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#/">進度查詢</a>
+                </li>
+                <li>
+                  <a href="#/">取消訂單</a>
+                </li>
+                <li>
+                  <a href="#/">追蹤清單</a>
+                </li>
+                <li>
+                  <a href="#/">常見問答</a>
+                </li>
+                <li>
+                  <a href="#/">聯絡我們</a>
+                </li>
+              </ul>
+            </div>
+            {/* Column4 */}
+            <div className="col-12 col-md-2">
+              <div className="font-700M footer-title">購物商城</div>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#/">生鮮食材</a>
+                </li>
+                <li>
+                  <a href="#/">調味料</a>
+                </li>
+                <li>
+                  <a href="#/">廚房器具</a>
+                </li>
+              </ul>
+            </div>
+            {/* Column5 */}
+            <div className="col-12 col-md-2">
+              <div className="font-700M footer-title">精選食譜</div>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#/">健康長肉肉</a>
+                </li>
+                <li>
+                  <a href="#/">健康不吃肉</a>
+                </li>
+                <li>
+                  <a href="#/">一周不煩惱</a>
+                </li>
+                <li>
+                  <a href="#/">家常好手藝</a>
+                </li>
+              </ul>
+            </div>
+            {/* Column6 */}
+            <div className="col-12 col-md-2">
+              <div className="font-700M footer-title">私藏食譜</div>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#/">熱門食譜</a>
+                </li>
+                <li>
+                  <a href="#/">上傳食譜</a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
 
-        <div className="footer-bottom">
-          <div className="d-flex align-items-center justify-content-between">
-            <div className="font-400SS col-6 pe-2">
-              本網站所有內容及資料全部來自網路，僅供學術交流使用，若有侵權請告知
-            </div>
-            <ul className="font-400SS list-unstyled col-6">
-              {bottom.map((v, i) => {
-                return (
-                  <li key={i}>
-                    <a href="#/">{v}</a>
-                  </li>
-                )
-              })}
-            </ul>
+        <div className="footer-bottom row">
+          <div className="font-400SS col-12 col-md-6 p-0">
+            本網站所有內容及資料全部來自網路，僅供學術交流使用，若有侵權請告知
           </div>
+          <ul className="font-400SS list-unstyled col-12 col-md-6">
+            {bottom.map((v, i) => {
+              return (
+                <li key={i}>
+                  <a href="#/">{v}</a>
+                </li>
+              )
+            })}
+          </ul>
         </div>
       </footer>
     </>
