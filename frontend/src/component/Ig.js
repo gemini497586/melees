5f09a9615c1e91e25cff1bc4d4e260a9f86d0764
ryanj8512 <ryanjan8512@gmail.com>
2021-09-08 16:11:47 +0800

    修改icon

--- frontend/src/component/Ig.js
+++ frontend/src/component/Ig.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import '../style/ig.css'
 import '../style/featureComponent.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './FontawsomeIcons'
 
 function Ig() {
   return (
     <>
       <div className="fintro-link">
         <div className="fintro-linkcontent">
-          <i className="fab fa-instagram fa-fw fintro-icon"></i>
+          <FontAwesomeIcon
+            className="fintro-icon"
+            icon={['fab', 'instagram']}
+            fixedWidth
+          />
           <div className="fintro-linkfont">
             <p className="m-0 pl-1 fcolor-secondary font-400SL">
               便當調色盤 | Della & Joey
