35a4373971949c6eb81de29921f3afe74a5819a9
meleelin <sdy2615380450@gmail.com>
2021-09-25 11:10:57 +0800

    修改後端api

--- backend/routers/private.js
+++ backend/routers/private.js
@@ -31,7 +31,11 @@ const uploader = multer({
   fileFilter: function (req, file, cb) {
     console.log("判斷副檔名", file.mimetype);
     // 判斷檔案的型態不是這三種的話，就 error
-    if (file.mimetype !== "image/jpg" && file.mimetype !== "image/png" && file.mimetype !== "image/jpeg") {
+    if (
+      file.mimetype !== "image/jpg" && 
+      file.mimetype !== "image/png" && 
+      file.mimetype !== "image/jpeg") 
+      {
       cb(new Error("不接受的檔案型態"), false);
     }
     cb(null, true);
@@ -45,9 +49,13 @@ const uploader = multer({
 
 // 抓食譜的資料 (食譜首頁)
 router.get("/index", async function (req, res, next) {
-  let memberId = 5;
-  let recipe = "SELECT * FROM private_recipe";
+  const userId = req.session.member ? req.session.member.id : ""
+  
+  let recipe = "SELECT * FROM private_recipe ORDER BY id DESC";
   let result = await connection.queryAsync(recipe);
+
+  let member = "SELECT * FROM member"
+  let memResult = await connection.queryAsync(member)
 
   let like = "SELECT private_id, count(*) as count FROM private_like GROUP BY private_id";
   let result2 = await connection.queryAsync(like);
@@ -55,90 +63,96 @@ router.get("/index", async function (req, res, next) {
   let view = "SELECT private_id, count(*) as count FROM private_view GROUP BY private_id";
   let result3 = await connection.queryAsync(view);
 
-  let saveState = "SELECT * FROM private_save WHERE user_id =?";
-  let result4 = await connection.queryAsync(saveState, [memberId]);
+  let saveState = "SELECT * FROM private_save WHERE user_id = ?";
+  let result4 = await connection.queryAsync(saveState, [userId]);
 
-  let likeState = "SELECT * FROM private_like WHERE user_id =?";
-  let result5 = await connection.queryAsync(likeState, [memberId]);
+  let likeState = "SELECT * FROM private_like WHERE user_id = ?";
+  let result5 = await connection.queryAsync(likeState, [userId]);
 
-  res.json({ result, result2, result3, result4, result5 });
+  res.json({ result, memResult, result2, result3, result4, result5 });
 });
 
 // 進入食譜的資料 (內頁)
-router.get("/index/:id?", async function (req, res, next) {
-  const memberId = 5;
+router.get("/index/recipe/:id", async function (req, res, next) {
+  const userId = req.session.member ? req.session.member.id : ""
+  
   let sql = "SELECT * FROM private_recipe WHERE id = ?";
   let result = await connection.queryAsync(sql, [req.params.id]);
 
+  let member = "SELECT * FROM member WHERE id = ?"
+  let memResult = await connection.queryAsync(member, [result[0].member_id])
+
   let sql2 = "SELECT * FROM private_follow WHERE user_id = ? AND private_id = ?";
-  let follow = await connection.queryAsync(sql2, [memberId, req.params.id]);
+  let follow = await connection.queryAsync(sql2, [userId, req.params.id]);
   let followed = follow.length > 0;
 
   let sql3 = "SELECT * FROM private_like WHERE user_id = ? AND private_id = ?";
-  let like = await connection.queryAsync(sql3, [memberId, req.params.id]);
+  let like = await connection.queryAsync(sql3, [userId, req.params.id]);
   let liked = like.length > 0;
 
   let sql4 = "SELECT * FROM private_save WHERE user_id = ? AND private_id = ?";
-  let save = await connection.queryAsync(sql4, [memberId, req.params.id]);
+  let save = await connection.queryAsync(sql4, [userId, req.params.id]);
   let saved = save.length > 0;
 
-  res.json({ result, followed, liked, saved });
+  res.json({ result, memResult, followed, liked, saved });
 });
 
 // 追蹤的新增或刪除
 router.get("/add-follow/:id", async function (req, res, next) {
-  let memberId = 5;
+  const userId = req.session.member.id
   let sql = "INSERT INTO private_follow (private_id, user_id) VALUES (?, ?)";
-  let follow = await connection.queryAsync(sql, [req.params.id, memberId]);
+  let follow = await connection.queryAsync(sql, [req.params.id, userId]);
 
   res.json(follow);
 });
 router.get("/remove-follow/:id", async function (req, res, next) {
-  let memberId = 5;
+  const userId = req.session.member.id
   let sql = "DELETE FROM private_follow WHERE private_id = ? AND user_id = ?";
-  let follow = await connection.queryAsync(sql, [req.params.id, memberId]);
+  let follow = await connection.queryAsync(sql, [req.params.id, userId]);
 
   res.json(follow);
 });
 
 // 按讚的新增或刪除
 router.get("/add-like/:id", async function (req, res, next) {
-  let memberId = 5;
+  const userId = req.session.member.id
+
+  // let memberId = 5;
   let sql = "INSERT INTO private_like (private_id, user_id) VALUES (?, ?)";
-  let like = await connection.queryAsync(sql, [req.params.id, memberId]);
+  let like = await connection.queryAsync(sql, [req.params.id, userId]);
 
   res.json(like);
 });
 router.get("/remove-like/:id", async function (req, res, next) {
-  let memberId = 5;
+  const userId = req.session.member.id
+
+  // let memberId = 5;
   let sql = "DELETE FROM private_like WHERE private_id = ? AND user_id = ?";
-  let like = await connection.queryAsync(sql, [req.params.id, memberId]);
+  let like = await connection.queryAsync(sql, [req.params.id, userId]);
 
   res.json(like);
 });
 
 // 按收藏的新增或刪除
 router.get("/add-save/:id", async function (req, res, next) {
-  let memberId = 5;
+  const userId = req.session.member.id
+  // let memberId = 5;
   let sql = "INSERT INTO private_save (private_id, user_id) VALUES (?, ?)";
-  let save = await connection.queryAsync(sql, [req.params.id, memberId]);
+  let save = await connection.queryAsync(sql, [req.params.id, userId]);
 
   res.json(save);
 });
 router.get("/remove-save/:id", async function (req, res, next) {
-  let memberId = 5;
+  const userId = req.session.member.id
+  // let memberId = 5;
   let sql = "DELETE FROM private_save WHERE private_id = ? AND user_id = ?";
-  let save = await connection.queryAsync(sql, [req.params.id, memberId]);
+  let save = await connection.queryAsync(sql, [req.params.id, userId]);
 
   res.json(save);
 });
 
 // 抓取食材的資料
 router.get("/ingred/:id", async function (req, res, next) {
-  // const memberId = req.session.member.id
-  const memberId = 5;
-  console.log("asdasdasd", memberId);
-  console.log("132132123");
   let sql = "SELECT * FROM private_ingred WHERE private_id = ?";
   let data = await connection.queryAsync(sql, [req.params.id]);
   console.log(data);
@@ -162,22 +176,161 @@ router.get("/tags/:id", async function (req, res, next) {
 // 抓取目前食譜的評論
 router.get("/comment/:id", async function (req, res, next) {
   let sql = "SELECT * FROM private_comment WHERE private_id = ?";
-  let data = await connection.queryAsync(sql, [req.params.id]);
-  res.json(data);
+  let result = await connection.queryAsync(sql, [req.params.id]);
+  let member = "SELECT * FROM member"
+  let memResult = await connection.queryAsync(member)
+  res.json({result, memResult});
 });
 
 // 更新瀏覽數
 router.get("/addview/:id", async function (req, res, next) {
-  let memberId = 5;
+  const userId = req.session.member ? req.session.member.id : ""
   let sql = "INSERT INTO private_view (private_id, user_id) VALUES (?, ?)";
-  let data = await connection.queryAsync(sql, [req.params.id, memberId]);
+  let data = await connection.queryAsync(sql, [req.params.id, userId]);
   res.send("success");
 });
 
+// 編輯食譜，先去資料庫拿資料
+router.get("/edit/get-data/:id", async function(req, res, next) {
+  console.log("123")
+  // const userId = req.session.member.id
+  let sql = "SELECT * FROM private_recipe WHERE id = ?"
+  let recipe = await connection.queryAsync(sql, [req.params.id])
+
+  let sql2 = "SELECT * FROM private_ingred WHERE private_id = ?"
+  let ingred = await connection.queryAsync(sql2, [req.params.id])
+
+  let sql3 = "SELECT * FROM private_step WHERE private_id = ?"
+  let steps = await connection.queryAsync(sql3, [req.params.id])
+  console.log(recipe)
+  console.log(ingred)
+  res.json({recipe, ingred, steps})
+})
+
+// 編輯食譜，編輯後送資料到資料庫
+router.post("/edit/post-data/:id", uploader.single("photo"), async function(req, res, next) {
+  const filename = req.file ? req.file.filename : "";
+  const name = req.body.name;
+  const intro = req.body.intro;
+  const qty = req.body.qty;
+  const newIngred = JSON.parse(req.body.ingred);
+  const newStep = JSON.parse(req.body.step);
+  if(filename === "") {
+    // 更新 private_recipe (沒有更改照片的)
+    let sql = "UPDATE private_recipe SET name = ?, intro = ?, qty = ? WHERE id = ?"
+    let result = await connection.queryAsync(sql, [name, intro, qty, req.params.id])
+    console.log("執行沒有照片的")
+  } else {
+    // 更新 private_recipe (有更改照片的)
+    let sql = "UPDATE private_recipe SET picture = ?, name = ?, intro = ?, qty = ? WHERE id = ?"
+    let result = await connection.queryAsync(sql, [filename, name, intro, qty, req.params.id])
+    console.log("執行有照片的")
+  }
+
+
+  // 先取出食材原本欄位
+  let sql2 = "SELECT * FROM private_ingred WHERE private_id = ?"
+  let oldIngred = await connection.queryAsync(sql2, [req.params.id])
+  let oldIngredCount = oldIngred.length
+  let newIngredCount = newIngred.length
+
+  let ingredRow = oldIngredCount - newIngredCount
+  // 如果 diff > 0 代表更新過後的欄位比較少
+  if (ingredRow > 0) {
+    let ingredRetain = oldIngred.slice(0, newIngredCount)
+    let ingredRemove = oldIngred.slice(newIngredCount)
+
+    // 更新食材到資料表
+    newIngred.forEach(async (value, index) => {
+      let ingredList = "UPDATE private_ingred SET ingred = ?, ingred_unit = ? WHERE id = ?"
+      let ingredResult = await connection.queryAsync(ingredList, [value.ingred, value.ingred_unit, ingredRetain[index].id]);
+    });
+
+    // 刪除多餘的欄位
+    ingredRemove.forEach(async (value, index) => {
+      let sql = "DELETE FROM private_ingred WHERE id = ?"
+      let test = await connection.queryAsync(sql, [value.id])
+    })
+  } else if (ingredRow < 0) {
+    // 如果 diff < 0 代表更新過後的欄位比較多
+    let ingredRetain = newIngred.slice(0, oldIngredCount)
+    let ingredInsert = newIngred.slice(oldIngredCount)
+
+    // 更新食材到資料表
+    ingredRetain.forEach(async (value,index) => {
+      let ingredList = "UPDATE private_ingred SET ingred = ?, ingred_unit = ? WHERE id = ?"
+      let ingredResult = await connection.queryAsync(ingredList, [value.ingred, value.ingred_unit, ingredRetain[index].id]);
+    })
+
+    // 新增多的欄位
+    ingredInsert.forEach(async (value, index) => {
+      let sql = "INSERT INTO private_ingred (private_id, ingred, ingred_unit) VALUES (?)"
+      let test = await connection.queryAsync(sql, [[req.params.id, value.ingred, value.ingred_unit]])
+    })
+  } else {
+    // 更新食材到資料表
+    newIngred.forEach(async (value, index) => {
+      let ingredList = "UPDATE private_ingred SET ingred = ?, ingred_unit = ? WHERE id = ?"
+      let ingredResult = await connection.queryAsync(ingredList, [value.ingred, value.ingred_unit, oldIngred[index].id]);
+    });
+  }
+  
+  
+  // 取出步驟原本欄位
+  let sql3 = "SELECT * FROM private_step WHERE private_id = ?"
+  let oldStep = await connection.queryAsync(sql3, [req.params.id])
+  let oldStepCount = oldStep.length
+  let newStepCount = newStep.length
+
+  let stepRow = oldStepCount - newStepCount
+  if(stepRow > 0) {
+    let stepRetain = oldStep.slice(0, newStepCount)
+    let stepRemove = oldStep.slice(newStepCount)
+
+    // 更新步驟到資料表
+    newStep.forEach(async (value, index) => {
+      let stepdList = "UPDATE private_step SET steps = ? WHERE id = ?"
+      let stepResult = await connection.queryAsync(stepdList, [value.steps, stepRetain[index].id]);
+    });
+
+    // 刪除原本剩餘的步驟
+    stepRemove.forEach(async (value, index) => {
+      let sql = "DELETE FROM private_step WHERE id = ?"
+      let test = await connection.queryAsync(sql, [value.id])
+    })
+  } else if (stepRow < 0) {
+    let stepRetain = newStep.slice(0, oldStepCount)
+    let stepInsert = newStep.slice(oldStepCount)
+
+    // 更新食材到資料表
+    stepRetain.forEach(async (value,index) => {
+      let stepList = "UPDATE private_step SET steps = ? WHERE id = ?"
+      let stepResult = await connection.queryAsync(stepList, [value.steps, stepRetain[index].id]);
+    })
+
+    // 新增多的欄位
+    stepInsert.forEach(async (value, index) => {
+      let sql = "INSERT INTO private_step (private_id, steps) VALUES (?)"
+      let test = await connection.queryAsync(sql, [[req.params.id, value.steps]])
+    })
+  } else {
+    // 更新食材到資料表
+    newStep.forEach(async (value, index) => {
+      let stepList = "UPDATE private_step SET steps = ? WHERE id = ?"
+      let ingredResult = await connection.queryAsync(stepList, [value.steps, oldStep[index].id]);
+    });
+  }
+  
+  
+    
+    
+
+})
 // 上傳食譜
 // 我只要上傳一張圖片，要加上 single
 // req.body 代表前端的 request 裡面全部的資料
 router.post("/upload/main", uploader.single("photo"), async function (req, res, next) {
+  const memberId = req.session.member.id
   const filename = req.file ? req.file.filename : "";
   const name = req.body.name;
   const intro = req.body.intro;
@@ -186,7 +339,6 @@ router.post("/upload/main", uploader.single("photo"), async function (req, res, 
   const steps = JSON.parse(req.body.steps);
   const time = moment().format("YYYY-MM-DD");
   const star_rate = 0;
-  const memberId = 5;
   const valid = 1;
 
   // 新增到 private_recipe 資料表裡
@@ -204,7 +356,7 @@ router.post("/upload/main", uploader.single("photo"), async function (req, res, 
     let ingredResult = await connection.queryAsync(ingredList, [[lastId[0].id, value.ingred, value.ingred_unit]]);
   });
 
-  // 新增食材到資料表
+  // 新增步驟到資料表
   steps.forEach(async (value) => {
     let stepList = "INSERT INTO private_step (private_id, steps) VALUES (?)";
     let stepResult = await connection.queryAsync(stepList, [[lastId[0].id, value.step]]);
@@ -215,10 +367,11 @@ router.post("/upload/main", uploader.single("photo"), async function (req, res, 
 
 // 上傳留言及評分部分
 router.post("/comment/upload/:id", async function (req, res, next) {
-  const memberId = 5;
+  const memberId = req.session.member.id
   const comment = req.body.comment;
+  const star_rate = req.body.starValue;
   const comment_time = moment().format("YYYY/MM/DD");
-  const star_rate = 3;
+  // const star_rate = 3;
 
   // 新增到 private_recipe 資料表裡
   let sql = "INSERT INTO private_comment (private_id, member_id, comment, star_rate, comment_time) VALUES (?)";
@@ -248,8 +401,8 @@ router.post("/comment/upload/:id", async function (req, res, next) {
 
 // 會員私藏食譜部分
 router.get("/myrecipe", async function (req, res, next) {
-  // const memberId = req.session.member.id
-  let memberId = 5;
+  const memberId = req.session.member.id
+  // let memberId = 5;
   console.log("memberId =", memberId);
   let sql = "SELECT * FROM private_recipe WHERE member_id = ?";
   let result = await connection.queryAsync(sql, [memberId]);
@@ -267,5 +420,6 @@ router.get("/myrecipe", async function (req, res, next) {
   let followResult = await connection.queryAsync(sql5, [memberId]);
   res.json({ result, commentResult, likeResult, viewResult, followResult });
 });
-
+// 先檢查是否已登入
+// router.use(loginCheckMiddleware);
 module.exports = router;
