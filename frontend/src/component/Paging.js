e376dc0b1e1493b89a5165893eb091680652fc80
bo-yuan026 <scsa8860512@gmail.com>
2021-09-27 14:30:11 +0800

    merge 冠霖的

--- frontend/src/component/Paging.js
+++ frontend/src/component/Paging.js
@@ -1,54 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import '../style/paging.css'
-import useCart from '../utils/useCart'
 
 function Paging(props) {
-  const { pageArray, setPageArray } = useCart()
-  // const [currentPage, setCurrentPage] = useState(1)
-  // const [perPage, setPerPage] = useState(10)
-  const { product, setCurrentPage } = props
-
-  let paging = Math.ceil(product.length / 10)
-
-  useEffect(() => {
-    let newPageArr = []
-    for (let i = 1; i < paging + 1; i++) {
-      newPageArr.push(i)
-    }
-    setPageArray(newPageArr)
-  }, [product])
-
-  // const lastNumber = currentPage * perPage
-  // const firstNumber = lastNumber - perPage
-  // const currentNumber = product.slice(firstNumber, lastNumber)
-  // console.log(currentNumber)
-
-  const handlePage = (e) => {
-    // console.log(e.target.classList)
-    setCurrentPage(e.target.innerHTML)
-    e.target.classList.add('active')
-  }
-
-  return (
-    <div className="paging">
-      {typeof pageArray === 'string' ? (
-        <></>
-      ) : (
-        pageArray.map((v) => {
-          return (
-            <button
-              className="btn paging-btn"
-              onClick={(e) => {
-                handlePage(e)
-              }}
-            >
-              {v}
-            </button>
-          )
-        })
-      )}
-    </div>
-  )
+  const { value } = props
+  return <button className="btn paging-btn active">{value}</button>
 }
 
 export default Paging
--- frontend/src/pages/market/MarketMainPage.js
+++ frontend/src/pages/market/MarketMainPage.js
@@ -3,7 +3,7 @@ import '../../style/marketMainPage.css'
 import ProductCard from './ProductCard'
 
 import SortingBar from './SortingBar'
-import Paging from '../../component/Paging'
+import Paging from './component/Paging'
 import MinorBar from './component/MinorBar'
 import CardRecipe from '../../component/CardRecipe'
 import { useParams } from 'react-router'
